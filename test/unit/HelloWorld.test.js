7f6f95c071e317096003fc1c9f3a500d42e5cf35
andres-geotec <andres.geotec@gmail.com>
2023-05-09 17:10:53 -0600

    testeo hello world

---
+++ test/unit/HelloWorld.test.js
@@ -0,0 +1,18 @@
+import { describe, expect } from 'vitest'
+import HelloWorld from "@/components/HelloWorld.vue";
+import { mount } from '@vue/test-utils';
+
+describe('HelloWorld.vue', () => {
+  test('debería representar el título correctamente', () => {
+    const msg = 'You’ve successfully created a project with'
+
+    const wrapper = mount(HelloWorld, {
+      props: {
+        msg
+      },
+    })
+
+    const h1 = wrapper.get('h1')
+    expect(h1.text()).toBe(msg)
+  })
+})
